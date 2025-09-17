b2a981e171c0de061dfcdd2af17a695f696ca9c6
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-09-18 04:35:17 +0600

    understand the catch all segment

---
+++ src/app/class/page.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ClassPage = () => {
+    return (
+        <div>
+            <h1>Thi is Main Class Page</h1>
+        </div>
+    );
+};
+
+export default ClassPage;
