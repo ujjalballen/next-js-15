5ebdcbfbb1b3316cc41c9c0dec9ef3c71fcd89f3
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-09-28 03:10:48 +0600

    understand Link

--- src/component/HomeLayout.js
+++ src/component/HomeLayout.js
@@ -1,9 +1,15 @@
+import Link from "next/link"
+
 export default function HomeLayout({children}){
 
     return(
         <>
         <header>
-            This is Home Header
+            <nav className="flex gap-2 item-center">
+                <Link href={'/'}>Home</Link>
+                <Link href={'/about'}>About</Link>
+                <Link href={'/counter'}>Counter</Link>
+            </nav>
         </header>
         <main>
             {children}
