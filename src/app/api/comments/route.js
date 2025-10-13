b880d5204084776c2dcc2ef095113c77feb9750d
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-10-13 22:52:10 +0600

    understand the POST request

--- src/app/api/comments/route.js
+++ src/app/api/comments/route.js
@@ -1,4 +1,22 @@
 import { comments } from "@/comments/data";
+
+
 export async function GET(){
 return Response.json(comments)
+}
+
+
+export async function POST(request){
+    const getData = await request.json();
+    const newComment = {
+        id: comments.length + 1,
+        text: getData.text
+    };
+
+    comments.push(newComment);
+
+    return new Response(JSON.stringify({message: "Comment Created:", newComment}), {
+        headers: {"Content-Type": "application/json"},
+        status: 201
+    })
 }
