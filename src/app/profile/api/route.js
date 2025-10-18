90ff22498e161a341f375a4b3bae0950f239571b
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-10-18 14:44:50 +0600

    understand the headers

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -1,3 +1,29 @@
-export async function GET(){
-    return new Response("user api profile")
+// export async function GET(){
+//     return new Response("user api profile")
+// }
+
+// ===>  Method 01: Get a Headers from the request
+
+// export async function GET(request){
+//     const requestHeaders = new Headers(request.headers)
+//     console.log("RequestHeaders: ", requestHeaders)
+//     const getHeader = requestHeaders.get('authorization');
+//     console.log(getHeader)
+
+//     return new Response("User API Profile")
+// }
+
+// ===>  Method 02: Get a Headers from the Next.js Headers
+
+import { headers } from 'next/headers'
+
+
+export async function GET(request) {
+  const headersList = await  headers();
+  const authorization = headersList.get("authorization");
+  console.log("authorization: ", authorization);
+
+  return new Response("<h1>user Api Profile</h1>", {
+    headers: {"Content-Type": "text/html"}
+  });
 }
