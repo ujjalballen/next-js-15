47f2d3173bb0cd548d9925f628198c5b6e05db9b
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-10-18 13:10:21 +0600

    Handling PATCH Requests

--- src/app/api/comments/[id]/route.js
+++ src/app/api/comments/[id]/route.js
@@ -5,4 +5,16 @@ export async function GET(request, {params}){
     const getComment = comments.find((comment) => comment.id === parseInt(id))
 
     return Response.json(getComment)
-}
+};
+
+export async function PATCH(request, {params}){
+    const {id} = await params;
+    const body = await request.json();
+    const {text} = body
+
+    const index = comments.findIndex(comment => comment.id === parseInt(id));
+    comments[index].text = text;
+
+    return Response.json(comments[index])
+};
+
