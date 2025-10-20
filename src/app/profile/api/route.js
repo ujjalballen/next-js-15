60c8564f9944db9468422f5d3d3020f44a8293ce
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-10-21 03:49:13 +0600

    Cookies in Route Handlers

--- src/app/profile/api/route.js
+++ src/app/profile/api/route.js
@@ -15,7 +15,7 @@
 
 // ===>  Method 02: Get a Headers from the Next.js Headers
 
-import { headers } from 'next/headers'
+import { headers, cookies } from 'next/headers'
 
 
 export async function GET(request) {
@@ -23,7 +23,19 @@ export async function GET(request) {
   const authorization = headersList.get("authorization");
   console.log("authorization: ", authorization);
 
+  // 1st methode to get the cookies from request
+  const theme = request.cookies.get('theme');
+  console.log("Theme Cookies: ", theme)
+
+  // 2nd build in Method
+  const cookieStore = await cookies();
+  cookieStore.set("snigdha", "Ujjal B. Allen")
+
+  const getSn = cookieStore.get("snigdha");
+  console.log("getSN: ", getSn)
+
+
   return new Response("<h1>user Api Profile</h1>", {
-    headers: {"Content-Type": "text/html"}
+    headers: {"Content-Type": "text/html", "Set-Cookie": "theme=dark"}
   });
 }
