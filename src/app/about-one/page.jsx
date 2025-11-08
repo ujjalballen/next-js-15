fab3781977d76fc990863033be1d870b20594ada
Ujjal B. Allen <ujjal.vision02@gmail.com>
2025-11-08 20:36:52 +0600

    Dynamic Rendering

--- src/app/about-one/page.jsx
+++ src/app/about-one/page.jsx
@@ -1,5 +1,13 @@
-export default function AboutOne(){
-    console.log("hello Server")
+// if i want to force to be a dynamic rendered, then we need to= export const dynamic = "force-dynamic"
+
+import {cookies} from 'next/headers'
+
+export default async function AboutOne(){
+
+    const cookieStore = await cookies();
+    const theme = cookieStore.get('theme');
+    console.log(theme)
+
     return (
         <h1>This is a server component</h1>
     )
